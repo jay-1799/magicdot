43a345272c094a4c2da5e475ddb4528b1767c73b
jay-1799 <patelj1799@gmail.com>
2025-01-09 19:35:59 -0500

    updated onsite routes

--- routes/onsite.js
+++ routes/onsite.js
@@ -34,4 +34,42 @@ router.route("/").get(async (req, res) => {
   }
 });
 
+router.route("/project/:id").get(async (req, res) => {
+  if (!req.session.user || req.session.user.role !== "onsite team") {
+    return res.redirect("/");
+  }
+  try {
+    const projectDetails = await projectData.getProjectById(req.params.id);
+    console.log(projectDetails.status);
+    var projectStatus = 0;
+
+    if (projectDetails.status == "approved") {
+      projectStatus = 1;
+    } else if (projectDetails.status == "site inspection") {
+      projectStatus = 2;
+    } else if (projectDetails.status == "inventory check") {
+      projectStatus = 3;
+    } else if (projectDetails.status == "under construction") {
+      projectStatus = 4;
+    } else if (projectDetails.status == "final inspection") {
+      projectStatus = 5;
+    } else if (projectDetails.status == "finished") {
+      projectStatus = 6;
+    } else {
+      throw "Project Status Invalid";
+    }
+
+    console.log(projectStatus);
+
+    return res.status(200).render("onsiteTeamProjectDetails", {
+      title: "Project Details",
+      projectDetails: projectDetails,
+      projectStatuses: projectStatus - 1,
+      projectTasksList: projectDetails.projectTasks,
+    });
+  } catch (error) {
+    return res.status(400).render("error", { error: error });
+  }
+});
+
 module.exports = router;
